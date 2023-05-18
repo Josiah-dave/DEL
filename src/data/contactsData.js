0abb9b8a65dcaad3a4c733014c42cc4fa490f0f0
Josiah David <josiahdavid111@gmail.com>
2023-05-18 18:29:09 +0100

    'checked'

---
+++ src/data/contactsData.js
@@ -0,0 +1,7 @@
+export const contactsData = {
+    email: 'josiahdavid111@gmail.com',
+    phone: '+915588776600',
+    address: 'Menlo Park, California, United States - 673822 ',
+
+    sheetAPI: 'https://sheetdb.io/api/v1/swaxxjlg5ldwj'
+}
